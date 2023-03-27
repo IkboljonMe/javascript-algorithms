df744b19b8bea97c4aa27432933f4925c5123316
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-03-28 00:50:12 +0200

    Linked List

---
+++ src/data-structures/linked-list/LinkedList.js
@@ -0,0 +1,148 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  add(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+  }
+
+  insert(value, index) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+
+    const newNode = new Node(value);
+
+    if (index === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else if (index === this.length) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      let current = this.head;
+      let previous = null;
+      let i = 0;
+
+      while (i < index) {
+        previous = current;
+        current = current.next;
+        i++;
+      }
+
+      newNode.next = current;
+      previous.next = newNode;
+    }
+
+    this.length++;
+    return true;
+  }
+
+  remove(value) {
+    let current = this.head;
+    let previous = null;
+
+    while (current) {
+      if (current.value === value) {
+        if (!previous) {
+          this.head = current.next;
+        } else {
+          previous.next = current.next;
+        }
+
+        if (!current.next) {
+          this.tail = previous;
+        }
+
+        this.length--;
+        return true;
+      }
+
+      previous = current;
+      current = current.next;
+    }
+
+    return false;
+  }
+
+  removeAt(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+
+    let current = this.head;
+    let previous = null;
+    let i = 0;
+
+    while (i < index) {
+      previous = current;
+      current = current.next;
+      i++;
+    }
+
+    if (!previous) {
+      this.head = current.next;
+    } else {
+      previous.next = current.next;
+    }
+
+    if (!current.next) {
+      this.tail = previous;
+    }
+
+    this.length--;
+
+    return current.value;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+
+    let current = this.head;
+    let i = 0;
+
+    while (i < index) {
+      current = current.next;
+      i++;
+    }
+
+    return current.value;
+  }
+
+  toArray() {
+    const arr = [];
+
+    let current = this.head;
+
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+
+    return arr;
+  }
+}
+
+module.exports = LinkedList;
---
+++ src/data-structures/linked-list/LinlkedList.test.js
@@ -0,0 +1,32 @@
+const LinkedList = require("./LinkedList");
+
+describe("LinkedList", () => {
+  let list;
+
+  beforeEach(() => {
+    list = new LinkedList();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+  });
+
+  it("should add a node to the end of the list", () => {
+    list.add(4);
+    expect(list.toArray()).toEqual([1, 2, 3, 4]);
+  });
+
+  it("should insert a node at the specified index", () => {
+    list.insert(4, 1);
+    expect(list.toArray()).toEqual([1, 4, 2, 3]);
+  });
+
+  it("should remove the first node with the specified value", () => {
+    list.remove(2);
+    expect(list.toArray()).toEqual([1, 3]);
+  });
+
+  it("should remove the node at the specified index", () => {
+    list.removeAt(1);
+    expect(list.toArray()).toEqual([1, 3]);
+  });
+});
